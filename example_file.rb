1c7b55a90599fe1170f718fa600d1d6a22eb8c99
Lauren Gifford <lauren.gifford.17@gmail.com>
2021-01-21 22:54:20 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,6 @@
+collection_name = {
+  :liverpool => "The Beatles",
+  :manchester => "The Smiths",
+  :coventry => "Delia",
+  :london => "Ziggy Stardust"
+}
---
+++ greeting.rb
@@ -0,0 +1,4 @@
+def greeting(name = 'neighbor')
+  puts "Hello #{name}"
+end
+greeting("Bob")
